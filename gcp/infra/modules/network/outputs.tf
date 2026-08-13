5227d60b47df578a935bab944bfb816d308e2edb
Rail Sharipov <hashan.hall@gmail.com>
2026-08-12 19:38:24 -0600

    update google cloud provider

--- gcp/infra/modules/network/outputs.tf
+++ gcp/infra/modules/network/outputs.tf
@@ -6,6 +6,10 @@ output "subnet_id" {
   value = google_compute_subnetwork.this.id
 }
 
+output "psc_endsubnet_id" {
+  value = google_compute_subnetwork.psc_endpoint_range.id
+}
+
 output "subnet_region" {
   value = google_compute_subnetwork.this.region
 }
